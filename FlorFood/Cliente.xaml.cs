8d40c1bd478ee5d96374abb19f7baaf2929f127b
Flor <alejandra.gaxiola11@gmail.com>
2023-05-18 18:45:15 -0700

    Se soluciono el problema de los frames

--- FlorFood/Cliente.xaml.cs
+++ FlorFood/Cliente.xaml.cs
@@ -9,16 +9,23 @@ public partial class Cliente : ContentPage
     public Cliente()
 	{
 		InitializeComponent();
-        AddContent();
+        pedidos.Add(CreateCustomFrame());
+        //AddContent();
     }
-    private void AddContent()
+
+    public Frame CreateCustomFrame()
     {
-        pedidos.Children.Clear();
         Frame frame = new Frame
         {
             BorderColor = Color.FromHex("#e33d8b"),
-            Padding = 10
+            Padding = new Thickness(10)
         };
+
+        StackLayout stackLayout = new StackLayout
+        {
+            Orientation = StackOrientation.Vertical
+        };
+
         Image image = new Image
         {
             Source = "hamburguesa.jpg",
@@ -26,16 +33,14 @@ public partial class Cliente : ContentPage
             HeightRequest = 150,
             WidthRequest = 150
         };
-        pedidos.Children.Add(image);
 
         Label label1 = new Label
         {
-            Text = "Hamburguesa",
+            Text = "Platillo 1",
             FontSize = 20,
             FontAttributes = FontAttributes.Bold,
             HorizontalOptions = LayoutOptions.Center
         };
-        pedidos.Children.Add(label1);
 
         Label label2 = new Label
         {
@@ -43,19 +48,18 @@ public partial class Cliente : ContentPage
             FontSize = 16,
             HorizontalOptions = LayoutOptions.Center
         };
-        pedidos.Children.Add(label2);
 
         StackLayout horizontalStackLayout = new StackLayout
         {
             Orientation = StackOrientation.Horizontal
         };
+
         Label label3 = new Label
         {
-            Text = "Precio: ",
+            Text = "                    Precio: ",
             FontSize = 16,
             HorizontalOptions = LayoutOptions.Center
         };
-        horizontalStackLayout.Children.Add(label3);
 
         Label label4 = new Label
         {
@@ -63,28 +67,112 @@ public partial class Cliente : ContentPage
             FontSize = 16,
             HorizontalOptions = LayoutOptions.Center
         };
-        horizontalStackLayout.Children.Add(label4);
-
-        pedidos.Children.Add(horizontalStackLayout);
 
         Button button = new Button
         {
             Text = "Agregar",
             BackgroundColor = Color.FromHex("#e33d8b"),
             HorizontalOptions = LayoutOptions.CenterAndExpand,
-            TextColor = Color.FromHex("#ffffff"),
+            TextColor = Colors.White,
             CornerRadius = 30,
             WidthRequest = 180,
             HeightRequest = 60,
             Margin = new Thickness(0, 0, 0, 10)
         };
-        pedidos.Children.Add(button);
 
-        frame.Content = pedidos;
+        stackLayout.Children.Add(image);
+        stackLayout.Children.Add(label1);
+        stackLayout.Children.Add(label2);
+        horizontalStackLayout.Children.Add(label3);
+        horizontalStackLayout.Children.Add(label4);
+        stackLayout.Children.Add(horizontalStackLayout);
+        stackLayout.Children.Add(button);
+        frame.Content = stackLayout;
 
-        Content = frame;
+        return frame;
     }
-    private void btnSalir_Clicked(object sender, EventArgs e)
+
+
+/*
+public Frame AddContent()
+{
+    Frame frame = new Frame
+    {
+        BorderColor = Color.FromHex("#e33d8b"),
+        Padding = 10
+    };
+    Image image = new Image
+    {
+        Source = "hamburguesa.jpg",
+        Aspect = Aspect.AspectFill,
+        HeightRequest = 150,
+        WidthRequest = 150
+    };
+    pedidos.Children.Add(image);
+
+    Label label1 = new Label
+    {
+        Text = "Hamburguesa",
+        FontSize = 20,
+        FontAttributes = FontAttributes.Bold,
+        HorizontalOptions = LayoutOptions.Center
+    };
+    pedidos.Children.Add(label1);
+
+    Label label2 = new Label
+    {
+        Text = "Descripción del platillo 1",
+        FontSize = 16,
+        HorizontalOptions = LayoutOptions.Center
+    };
+    pedidos.Children.Add(label2);
+
+    StackLayout horizontalStackLayout = new StackLayout
+    {
+        Orientation = StackOrientation.Horizontal
+    };
+    Label label3 = new Label
+    {
+        Text = "Precio: ",
+        FontSize = 16,
+        HorizontalOptions = LayoutOptions.Center
+    };
+    horizontalStackLayout.Children.Add(label3);
+
+    Label label4 = new Label
+    {
+        Text = "150",
+        FontSize = 16,
+        HorizontalOptions = LayoutOptions.Center
+    };
+    horizontalStackLayout.Children.Add(label4);
+
+    pedidos.Children.Add(horizontalStackLayout);
+
+    Button button = new Button
+    {
+        Text = "Agregar",
+        BackgroundColor = Color.FromHex("#e33d8b"),
+        HorizontalOptions = LayoutOptions.CenterAndExpand,
+        TextColor = Color.FromHex("#ffffff"),
+        CornerRadius = 30,
+        WidthRequest = 180,
+        HeightRequest = 60,
+        Margin = new Thickness(0, 0, 0, 10)
+    };
+    pedidos.Children.Add(button);
+
+    frame.Content = pedidos;
+
+    Content = frame;
+
+    return frame;
+}*/
+
+
+
+
+private void btnSalir_Clicked(object sender, EventArgs e)
     {
         Navigation.PopAsync();
     }
